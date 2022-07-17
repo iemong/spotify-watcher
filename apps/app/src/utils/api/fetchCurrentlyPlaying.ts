70ca8d2b873148239261d8f4f132a71551cc21dc
iemong <kiyoshidainagon@gmail.com>
2022-07-17 23:53:11 +0900

    fix fetcher

--- apps/app/src/utils/api/fetchCurrentlyPlaying.ts
+++ apps/app/src/utils/api/fetchCurrentlyPlaying.ts
@@ -1,3 +1,17 @@
+import { camelizeKeys } from "@/utils/camelizeKeys";
+import { CamelCasedPropertiesDeep } from "type-fest";
+
+type RawCurrentTrack = {
+  item: {
+    album: {
+      name: string;
+      images: Array<{ width: number; height: number; url: string }>;
+    };
+  };
+};
+
+type CurrentTrack = CamelCasedPropertiesDeep<RawCurrentTrack>;
+
 export const fetchCurrentlyPlaying = async (accessToken: string) => {
   const res = await fetch(
     "https://api.spotify.com/v1/me/player/currently-playing",
@@ -9,5 +23,19 @@ export const fetchCurrentlyPlaying = async (accessToken: string) => {
       },
     }
   );
-  return await res.json();
+  try {
+    const data = await res.json();
+    return camelizeKeys(data) as CurrentTrack;
+  } catch (err) {
+    console.error(err);
+    const data = {
+      item: {
+        album: {
+          name: "",
+          images: [{ url: "" }],
+        },
+      },
+    };
+    return camelizeKeys(data) as CurrentTrack;
+  }
 };
